458bfa6b0b1905f345ffd7f1a1c2f49f310edca4
guinevere <kalogjeraa@gmail.com>
2021-01-05 13:40:05 -0500

    Create navbar and button components + styles.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hi
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
